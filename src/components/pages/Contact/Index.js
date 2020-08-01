84d3d6a801b90939d38126b4ce95f49213f984e5
Aleksandr Saiyan <anverch@gmail.com>
2020-07-31 19:21:50 -0500

    update conact page

--- src/components/pages/Contact/Index.js
+++ src/components/pages/Contact/Index.js
@@ -4,10 +4,12 @@ import "./style.css";
 
 
 function Contact() {
+  // 5 states, 1 for each variable
   const [name, setName]=useState("");
   const [email, setEmail]=useState("");
   const [message, setMessage]=useState("");
   const [errMsg, setErrMsg]=useState("");
+  const [sentMsg, setSentMsg]=useState("");
 
   function handleNameChange(e){
     setName(e.target.value)
@@ -25,11 +27,14 @@ function Contact() {
     e.preventDefault();
 
     if (!name || !email || !email.includes("@") || !message){
-      setErrMsg("All information needs to be provided")
+      setErrMsg("All information needs to be provided");
       return
     } 
-    console.log(name, email, message);
-    
+    setErrMsg("");
+    setSentMsg("Message has been sent");
+    setName("");
+    setEmail("");
+    setMessage("");
   }
 
     return ( 
@@ -45,20 +50,23 @@ function Contact() {
                   <div className="col-md-12">
                     {/*adding contact box*/}
                     {
-                      errMsg && <div>{errMsg}</div>
+                      errMsg && <div className="text-danger">{errMsg}</div>
+                    }
+                    {
+                      sentMsg && <div className="text-success">{sentMsg}</div>
                     }
                     <form>
                       <div className="form-group">
                         <label htmlFor="exampleFormControlInput1">Name</label>
-                        <input type="name" onChange={handleNameChange} className="form-control" id="exampleFormControlInput1" placeholder="Name" />
+                        <input type="name" onChange={handleNameChange} value={name} className="form-control" id="exampleFormControlInput1" placeholder="Name" />
                       </div>
                       <div className="form-group">
                         <label htmlFor="exampleFormControlInput1">Email address</label>
-                        <input type="email" onChange={handleEmailChange} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        <input type="email" onChange={handleEmailChange} value={email} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                       </div>
                       <div className="form-group">
                         <label htmlFor="exampleFormControlTextarea1">Message</label>
-                        <textarea onChange={handleMessageChange} className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+                        <textarea onChange={handleMessageChange} value={message} className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
                         <br />
                         <button onClick={handleSubmit} className="btn-primary">Submit</button>
                       </div>
